7225805d855425bc0b95233d6a413aa109d17478
blessyhema56-cmyk <blessy.hema56@gmail.com>
2026-02-06 09:32:55 +0530

    file change

---
+++ day 10 6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char username[] = "admin";
+    char password[] = "1234";
+
+    if(strcmp(username, "admin") == 0 && strcmp(password, "1234") == 0)
+        printf("Login successful");
+    else
+        printf("Login failed");
+
+    return 0;
+}
+OUTPUT
+Login successful
