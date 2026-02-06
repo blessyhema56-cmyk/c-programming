10b18b8f46c8575c11d40ec523f407c32e61dbfe
blessyhema56-cmyk <blessy.hema56@gmail.com>
2026-02-06 09:31:41 +0530

    file changes

---
+++ day 10 p5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int roll[] = {101, 102, 103, 101};
+    int n = 4;
+    int found = 0;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(roll[i] == roll[j]) {
+                found = 1;
+                break;
+            }
+        }
+    }
+
+    if(found)
+        printf("Duplicate found");
+    else
+        printf("No duplicate");
+
+    return 0;
+}
+OUTPUT
+Duplicate found
